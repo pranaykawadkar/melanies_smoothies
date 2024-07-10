612af594b9b3d7a5386285fb6c828c7c0ce964f6
pranaykawadkar <127779062+pranaykawadkar@users.noreply.github.com>
2024-07-10 16:08:06 +0530

    Update streamlit_app.py
    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
 
         # Example API call for each selected fruit (here it's hardcoded to "watermelon" for demonstration)
