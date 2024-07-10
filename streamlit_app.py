f853bcde68ba73374905cd93704d16d15fefe4db
pranaykawadkar <127779062+pranaykawadkar@users.noreply.github.com>
2024-07-10 15:08:56 +0530

    Update streamlit_app.py
    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,19 +21,19 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        # Example API call for each selected fruit (here it's hardcoded to "watermelon" for demonstration)
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen.lower())
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
     my_insert_stmt = f"""insert into smoothies.public.orders(ingredients, name_on_order)
-                         values ('{ingredients_string.strip()}', '{name_on_order}')"""
+                        values ('{ingredients_string.strip()}', '{name_on_order}')"""
 
     st.write(my_insert_stmt)
+    st.stop()
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+    session.sql(my_insert_stmt).collect()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    st.success('Your Smoothie is ordered!', icon="✅")
