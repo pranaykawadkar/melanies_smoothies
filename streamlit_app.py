f12a66859eb9847d8f4fec30e8849f69d2f91537
pranaykawadkar <127779062+pranaykawadkar@users.noreply.github.com>
2024-07-10 15:13:34 +0530

    Update streamlit_app.py
    Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+    # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -21,9 +21,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
 
         # Example API call for each selected fruit (here it's hardcoded to "watermelon" for demonstration)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen.lower())
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
